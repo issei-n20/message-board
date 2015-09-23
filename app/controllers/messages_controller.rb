4aad797ec10ec78e61aa9eff397332b35bb430e6
issei_n20 <nema.irm@gmail.com>
2015-09-23 07:03:18 +0000

    messages_destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_msessage, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     #Messageを全て取得する
     @messages = Message.all
@@ -18,14 +18,22 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
   def update
-    if @message.update(message.params)
+    if @message.update(message_params)
       #保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
     else
       #保存に失敗した場合
       render 'edit'
     end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
