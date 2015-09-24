076d130650c8c7620c4873f2bfe78dde9126cea8
issei_n20 <nema.irm@gmail.com>
2015-09-24 09:16:22 +0000

    challenge2

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921085450) do
+ActiveRecord::Schema.define(version: 20150923081436) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
